8b799d1dbbc550c9956c70cf842386861ace8163
lwieske <lothar.wieske@web.de>
2018-02-17 18:35:23 +0100

    Update Build 17025

---
+++ choco-packs.ps1
@@ -0,0 +1,39 @@
+choco install chocolateygui          --confirm
+
+choco install jdk9                   --confirm
+choco install ant                    --confirm
+choco install maven                  --confirm
+choco install gradle                 --confirm
+
+choco install nodejs                 --confirm
+choco install yarn                   --confirm
+
+choco install miniconda3             --confirm
+choco install anaconda3              --confirm
+
+choco install ruby                   --confirm
+
+choco install docker-toolbox         --confirm
+choco install docker                 --confirm
+
+choco install kubernetes-cli         --confirm
+choco install etcd                   --confirm
+choco install minikube               --confirm
+
+choco install openshift-cli          --confirm
+choco install minishift              --confirm
+
+choco install github-desktop         --confirm
+choco install eclipse                --confirm
+choco install intellijidea-community --confirm
+choco install visualstudiocode       --confirm
+
+choco install awscli                 --confirm
+choco install azure-cli              --confirm
+
+choco install wget                   --confirm
+choco install packer                 --confirm
+choco install vagrant                --confirm
+choco install terraform              --confirm
+
+exit 0
--- guest-tools.ps1
+++ guest-tools.ps1
@@ -1,3 +1,3 @@
-certutil -addstore -f 'TrustedPublisher' 'E:\cert\oracle-vbox.cer'
+E:\cert\VBoxCertUtil.exe add-trusted-publisher E:\cert\vbox-sha256.cer --root E:\cert\vbox-sha256-r3.cer
 
-Start-Process 'E:\VBoxWindowsAdditions.exe' '/S' -Wait
+Start-Process 'E:\VBoxWindowsAdditions-amd64.exe' '/S' -Wait
