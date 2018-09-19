79b8c70c5cc65882d5ad96c23ab08683d24d92ce
lwieske <lothar.wieske@web.de>
2018-09-19 15:45:03 +0200

    Redstone 5 / Build  17744

--- choco-packs.ps1
+++ choco-packs.ps1
@@ -1,39 +1,27 @@
-choco install chocolateygui          --confirm
+choco install chocolateygui             --confirm
 
-choco install jdk9                   --confirm
-choco install ant                    --confirm
-choco install maven                  --confirm
-choco install gradle                 --confirm
+choco install maven                     --confirm
+choco install gradle                    --confirm
 
-choco install nodejs                 --confirm
-choco install yarn                   --confirm
+choco install nodejs                    --confirm
+choco install yarn                      --confirm
 
-choco install miniconda3             --confirm
-choco install anaconda3              --confirm
+choco install docker                    --confirm
 
-choco install ruby                   --confirm
+choco install kubernetes-cli            --confirm
+choco install etcd                      --confirm
+choco install minikube                  --confirm
 
-choco install docker-toolbox         --confirm
-choco install docker                 --confirm
+choco install atom                      --confirm
+choco install github-desktop            --confirm
+choco install visualstudiocode          --confirm
 
-choco install kubernetes-cli         --confirm
-choco install etcd                   --confirm
-choco install minikube               --confirm
+choco install awscli                    --confirm
+choco install azure-cli                 --confirm
+choco install terraform                 --confirm
 
-choco install openshift-cli          --confirm
-choco install minishift              --confirm
-
-choco install github-desktop         --confirm
-choco install eclipse                --confirm
-choco install intellijidea-community --confirm
-choco install visualstudiocode       --confirm
-
-choco install awscli                 --confirm
-choco install azure-cli              --confirm
-
-choco install wget                   --confirm
-choco install packer                 --confirm
-choco install vagrant                --confirm
-choco install terraform              --confirm
+choco install wget                      --confirm
+choco upgrade insomnia-rest-api-client  --confirm
+choco install jq                        --confirm
 
 exit 0
